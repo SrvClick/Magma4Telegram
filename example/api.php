1a0c0ff669f9bcd72bea0186530bea0f20fde6b8
srvclick <admin@srvclick.com>
2024-03-30 05:42:38 -0600

    Magma

--- example/api.php
+++ example/api.php
@@ -4,6 +4,7 @@ require_once __DIR__."/../vendor/autoload.php";
 require_once __DIR__."/Modules/init.php";
 use Srvclick\Magma4telegram\Magma;
 use Modules\info;
+
 Magma::$magma = [
     info::class
 ];

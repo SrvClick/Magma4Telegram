1e04e12c01b73b48b40af1ba0a39ce9759af554f
srvclick <admin@srvclick.com>
2024-03-30 08:25:42 -0600

    Magma

--- src/Magma.php
+++ src/Magma.php
@@ -12,50 +12,67 @@ use ReflectionClass;
  * @contact: https://www.srvclick.com
  * @version: 1.0 BETA - 30/03/2024
  */
-class Magma extends MagmaKernel{
+class Magma extends MagmaKernel {
+    private static array $reflection = [];
 
     /**
      * @throws Exception
      */
-    public function __construct($command){
-        if (!isset($command) or empty($command)) throw new Exception("Invalid Command");
-        $command = $this->parser($command);
-        $name = $command['command'];
-        $arg = [];
-       foreach (self::$magma as $class){
-           try {
-           $magma = new ReflectionClass($class);
-           $classCommand = $magma->getProperty('command')->getDefaultValue();
-           $userCommand = $this->parser($classCommand);
-
-           if ($name == $userCommand['command']){
-               if (count($userCommand['args']) > 0){
-                   if (count($userCommand['args']) != count($command['args'])) throw new Exception('Cantidad de parametros invalidos');
-                   for ($i = 0; $i < count($userCommand['args']); $i++){
-                       $arg[$userCommand['args'][$i]] = $command['args'][$i];
-                   }
-               }
-               $app = new $magma->name;
-               $app->setArguments( $arg );
-               $app->handle();
-               break;
-             }
-           } catch (ReflectionException $e) {
-               echo $e->getMessage();
-           }
-       }
-
-    }
-    public function parser($command): array
-    {
-        $parser = explode(" ",$command);
-        $args = [];
-        foreach ($parser as $arg){
-            $tmparg = str_replace(['{','}'],null,$arg);
-            if ($tmparg == $parser[0]) continue;
-            $args[] = $tmparg;
+    public function __construct($command) {
+        if (!isset($command) || empty($command)) {
+            throw new Exception("Invalid Command");
         }
+
+        $commandData = $this->parser($command);
+        $name = $commandData['command'];
+        $arg = [];
+
+        foreach (self::$magma as $class) {
+            try {
+                $reflection = $this->getCachedReflection($class);
+                $classCommand = $reflection->getProperty('command')->getDefaultValue();
+                $userCommand = $this->parser($classCommand);
+
+                if ($name == $userCommand['command']) {
+                    $this->validateArguments($commandData['args'], $userCommand['args']);
+
+                    foreach ($userCommand['args'] as $index => $argName) {
+                        $arg[$argName] = $commandData['args'][$index];
+                    }
+
+                    $app = $reflection->newInstance();
+                    $app->setArguments($arg);
+                    $app->handle();
+                    return;
+                }
+            } catch (ReflectionException $e) {
+            }
+        }
+    }
+
+    private function getCachedReflection($class) {
+        if (!isset(self::$reflection[$class])) {
+            self::$reflection[$class] = new ReflectionClass($class);
+        }
+        return self::$reflection[$class];
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function validateArguments($providedArgs, $expectedArgs): void
+    {
+        if (count($providedArgs) != count($expectedArgs)) {
+            throw new Exception('Invalid number of parameters');
+        }
+    }
+
+    public function parser($command): array {
+        $parser = explode(" ", $command);
+        $args = array_map(function($arg) {
+            return trim($arg, '{}');
+        }, array_slice($parser, 1));
         $command = $parser[0];
-        return compact('command','args');
+        return compact('command', 'args');
     }
 }
